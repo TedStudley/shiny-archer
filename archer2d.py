348b63c8a5a48dbf5b0b9593025805e83829c60c
tedstudley <tedstudley@15ba8860-dd19-454a-b621-e8934a91a441>
2013-08-16 23:33:16 +0000

    Added (non-working) 2d script
    git-svn-id: http://svn-egp.math.ucdavis.edu/usvn/svn/CIG_ASPECT_OUWG/trunk/shiny-archer/trunk@5 15ba8860-dd19-454a-b621-e8934a91a441

--- archer2d.py
+++ archer2d.py
@@ -1,26 +1,28 @@
 from dolfin import *
 import sys
 
-N       = 256
-sigma   = 0.9
-epsilon = 1.25
+N       = 64
+sigma   = 0.25
+epsilon = 0.125
 T       = 1
-c       = 0.6
+c       = 0.5
 h = 1.0 / N
 dt = h * 0.001
 iteration = 0
 
 info("Initial parameters: N = {0}; sigma = {1}; epsilon = {2}; h = {3}; dt = {4}".format(N, sigma, epsilon, h, dt))
 
-mesh = UnitIntervalMesh(N)
+mesh = UnitSquareMesh(N, N)
 V = FunctionSpace(mesh, 'Lagrange', 2)
 
-u0 = Expression('(0.25 < x[0] && x[0] < 0.75) ? 1 : 0')
+u0 = Expression('((0.375 < x[0] && x[0] < 0.625) && (0.375 < x[1] && x[1] < 0.625)) ? 1 : 0')
 
 def boundary(x, on_boundary):
   tol = 1E-15
   return abs(x[0]) < tol or \
-         abs(x[0] - 1) < tol
+         abs(x[1]) < tol or \
+         abs(x[0] - 1) < tol or \
+         abs(x[1] - 1) < tol
 
 bc = DirichletBC(V, Constant(0.0), boundary)
 
@@ -32,6 +34,9 @@ v = TestFunction(V)
 a = u*v*dx + epsilon*dt*c*inner(nabla_grad(u), nabla_grad(v))*dx
 
 A = assemble(a)
+
+print A.array()
+
 u = Function(V)
 t = dt
 
@@ -39,9 +44,10 @@ while t <= T:
   L = u_1*v*dx + epsilon*dt*(1-c)*inner(nabla_grad(u_1), nabla_grad(v))*dx
   b = assemble(L)
   bc.apply(A, b)
-  solve(A, u.vector(), b)
+  solve(A, u.vector(), b, "gmres", "ilu")
   info("Iteration {0} (Time {1}): Min = {2}; Max = {3}".format(iteration, t, u.vector().min(), u.vector().max()))
   plot(u)
+  interactive()
   u_1.assign(u)
   t += dt
   iteration += 1
